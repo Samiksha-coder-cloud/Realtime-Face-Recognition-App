71957ed1dfa0104791079d7e013164e54e8155a4
Samiksha-coder-cloud <78074734+Samiksha-coder-cloud@users.noreply.github.com>
2021-09-13 16:51:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,29 @@ function preload() {
 function setup() {
     canvas = createCanvas(300,260);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/CONKQ0bCc/model.json", modelLoaded);
 }
 
 function draw() {
+    image(video, 0, 0, 300, 260);
+    classifier.classify(video, gotResult);
+}
 
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } 
+    else {
+        console.log(results);
+
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy_name").innerHTML = results[0].confidence.toFixed(4);
+    }
 }
